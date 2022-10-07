51df48d51356e38f25900f772e6a7b9d2083b44b
seoanchoi <seoaneeee@naver.com>
2022-10-07 09:15:18 +0900

    문제1

--- java_demo02/src/kr/com/exam/Exam01.java
+++ java_demo02/src/kr/com/exam/Exam01.java
@@ -15,16 +15,16 @@ public class Exam01 {
 		// 4. 변수에 저장된 모든 값을 .println() 함수로 출력 하세요.
 		System.out.println(name);
 		System.out.println(age);
-		System.out.println(talle);
+		System.out.println(tall);
 		
 		// 5. 변수에 저장된 값을 활용하여 다음 형식에 맞추어 출력하세요.
 		//    형식에 맞추어 출력 할 때 .print() / .printf() / .println() 중 
 		//    자신이 사용하기 편한 함수 하나를 선택하여 출력하세요.
 		//	  형식: 저의 이름은 홍길동 입니다. 올해 30세 이며, 내년에는 31세가 됩니다. 
 		System.out.print("저의 이름은 " + name + " 입니다. 올해 ");
-		System.out.print(age + "세 이며, 내년에는 (age + 1) + "세가 됩니다.\n");
+		System.out.print(age + "세 이며, 내년에는" + (age + 1) +"세가 됩니다.\n");
 		
-		System.out.println("저의 이름은 " + name + " 입니다. 올해 " + age + "세 이며, 내년에는 (age + 1) + "세가 됩니다.\n");"
+		System.out.println("저의 이름은 " + name + " 입니다. 올해 " + age + "세 이며, 내년에는" + (age + 1) + "세가 됩니다.");
 				
 		System.out.printf("저의 이름은 %s 입니다. 올해 %d세 이며, 내년에는 %d세가 됩니다.", name, age, age + 1);
 	}
