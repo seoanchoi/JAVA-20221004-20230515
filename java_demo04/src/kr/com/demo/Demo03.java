c9b2ced7b0d36d1b818a5bb530ef145cac7b1551
seoanchoi <seoaneeee@naver.com>
2022-10-11 15:13:02 +0900

    switch 문

---
+++ java_demo04/src/kr/com/demo/Demo03.java
@@ -0,0 +1,64 @@
+package kr.com.demo;
+
+public class Demo03 {
+
+	public static void main(String[] args) {
+		/*
+		 *  switch 문
+		 *  	if 문과 비슷하게 조건에 따라 특정 코드가 실행 되도록 하는 문장
+		 *  	
+		 *  	if 문의 경우 크기 비교 연산등을 활용하여 
+		 *  	특정 값의 범위에 해당하는 경우를 선택할 수 있으나
+		 *  	switch 문의 경우 연산 결과에 해당하는 값인 경우에만 
+		 *  	선택적으로 실행되게 활 수 있다.
+		 *  
+		 *  Switch(연산식) {
+		 *  	case 값1:
+		 *  		실행 코드
+		 *  		break;
+		 *  	case 값2:
+		 *  		실행 코드
+		 *  	case 값3:
+		 *  		실행 코드
+		 *  	default:
+		 *  		실행 코드 
+		 *  }
+		 *  
+		 *  switch 문에서 default 는 생략 가능하며, case 에 해당하지 않는 값이계산된 경우 실행.
+		 *  
+		 *  switch 문에서 각 case 마다 마지막 줄에 break 를 사용하지 않으면
+		 *  다음 case가 동작 된다.(break 를 사용하여 switch 문에 대한 중지를 할 수 있다.)
+		 */
+		
+		
+		int value = 3;
+		
+		switch(value) {
+			case 1:
+				System.out.println("case 1 에 해당되어 동작!");
+				break;
+			case 2:
+				System.out.println("case 2 에 해당되어 동작!");
+				// break;
+			case 3:
+				System.out.println("case 3 에 해당되어 동작!");
+				break;
+			default:
+				System.out.println("어떠한 case 에도 해당되지 않아 동작!");
+		
+		
+		}
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+
+}
