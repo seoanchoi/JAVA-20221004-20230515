68e2b4b260a4c99ae8ce7f20147c426354a8fbcc
seoanchoi <seoaneeee@naver.com>
2022-10-12 15:22:47 +0900

    연습문제

---
+++ java_demo05/src/java_demo05/Exam02.java
@@ -0,0 +1,30 @@
+package java_demo05;
+
+import java.util.Scanner;
+
+public class Exam02 {
+
+	public static void main(String[] args) {
+		/* 
+		 * Exa02.java 파일을 생성 후 다음의 문제를 풀어보세요.
+		 * 사용자 입력으로 5 ~ 10 사이의 정수 값을 입력 받아
+		 * 입력 받은 정수값과 동일한 크기의 배열을 생성하세요.
+		 * 그리고 배열의 값은 -1 로 초기화 하세요.
+		 */
+		
+	Scanner sc = new Scanner(System.in);	
+	
+	System.out.print("정수값 입력 : ");
+	int x = sc.nextInt();
+	
+	
+	
+	}
+
+}
+int num = 0;  
+for(int cnt = 1; cnt <=3; cnt++) {
+	System.out.print("1 ~ 45 사이의 정수 입력 : ");
+	
+	
+	
---
+++ java_demo05/src/java_demo05/Exam04.java
@@ -0,0 +1,12 @@
+package java_demo05;
+
+public class Exam04 {
+
+	public static void main(String[] args) {
+		/*
+		 * Exam03.java 와 동일한 과정으로 배열을 생성할 때
+		 * 중복된 값이 없도록 초기화 하세요.
+		 */
+	}
+
+}
