3752c3b172474bf43980995ec06f4921545ac29d
seoanchoi <seoaneeee@naver.com>
2022-10-19 09:10:05 +0900

    Random 클래스

---
+++ java_demo05/src/kr/co/demo/Demo02.java
@@ -0,0 +1,26 @@
+package kr.co.demo;
+
+import java.util.Random;
+
+public class Demo02 {
+
+	public static void main(String[] args) {
+		/*
+		 * Random 클래스
+		 * 		난수값을 생성하기 위한 클래스
+		 * 
+		 */
+		Random random = new Random();
+		int lotto[] = new int[6];
+		
+		for(int i = 0; i < 6; i++) {
+			int num = random.nextInt(45) + 1; // +1 하지 않으면 0 ~ 44
+			lotto[i] = num;
+		}
+		
+		for(int i = 0; i < 6; i++) {
+			System.out.printf("%d\t", lotto[i]);
+		}
+		System.out.println();
+	}
+}
