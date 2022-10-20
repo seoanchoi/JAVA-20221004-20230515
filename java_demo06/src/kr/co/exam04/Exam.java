33f30ce52e34bcb0f785e58c568a50293feac090
seoanchoi <seoaneeee@naver.com>
2022-10-20 15:13:31 +0900

    실행 메뉴 만들어서 동작시키기

--- java_demo06/src/kr/co/exam04/Exam.java
+++ java_demo06/src/kr/co/exam04/Exam.java
@@ -1,7 +1,5 @@
 package kr.co.exam04;
 
-import java.util.Arrays;
-
 public class Exam {
 
 	public static void main(String[] args) {
@@ -15,42 +13,10 @@ public class Exam {
 		 *         멤버변수 : 과목명, 점수, 등급
 		 *         멤버메서드 : 점수로 등급을 산출하는 메서드
 		 */
-		Student s = new Student("홍길동");
-
-		String table = s.getGradeTable();
-		System.out.print(table);
-		
-		double score = s.getScore("과학");
-		System.out.println(score);
-		
-		s.addSubject("과학", 76.5);
-		s.addSubject("국어", 76.5);
-		s.addSubject("수학", 76.5);
-		s.addSubject(new Subject("영어", 67));
-		table = s.getGradeTable();
-		System.out.print(table);
-		
-		s.updateSubject("과학", 95);
-		table = s.getGradeTable();
-		System.out.print(table);
-		
-		System.out.println("----------");
-		
-		s.removeSubject("수학");
-		table = s.getGradeTable();
-		System.out.print(table);
-		
-		Subject s1 = s.getSubject("영어");
-		System.out.print(s1.getName() + "\t");
-		System.out.print(s1.getScore() + "\t");
-		System.out.print(s1.getGrade() + "\n");
-		
-		Subject sArr[] = s.getSubjects("영어", "국어", "수학", "과학");
-		System.out.println(Arrays.toString(sArr));
 
 		
 		/*
-		 * Run 클레스를 만들고 지금까지 만들어 둔 Student 클래스를 사용하여 
+		 * Run 클래스를 만들고 지금까지 만들어 둔 Student 클래스를 사용하여 
 		 * 다음의 프로그램을 만들어 보세요
 		 * 
 		 * 학생의 성적을 관리하기 위한 프로그램
@@ -71,6 +37,8 @@ public class Exam {
 		 * 초기 메뉴로 들아가려면  Enter 입력!
 		 * !
 		 */
+		
+		
 		
 		/* 메뉴 번호 입력 : 3
 		 * 
@@ -107,7 +75,8 @@ public class Exam {
 		 * 초기 메뉴로 돌아가려면 Enter 입력! 
 		 */
 
-		
+		Run r = new Run();
+		r.start();		
 		
 		
 	}
