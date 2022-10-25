7302b8fc58b3276528aa65508a8135dc1014b83e
seoanchoi <seoaneeee@naver.com>
2022-10-25 11:29:59 +0900

    toString() 적용

--- java_demo07/src/kr/co/demo02/Person.java
+++ java_demo07/src/kr/co/demo02/Person.java
@@ -1,7 +1,9 @@
 package kr.co.demo02;
 
+import java.util.Objects;
+
 public class Person {
-	
+
 	private String name;
 	private int age;
 	private char gender;
@@ -32,18 +34,23 @@ public class Person {
 	
 	@Override
 	public String toString() {
-		return String.format("Person[name: %s, age: %d]", this.name, this.age);
+		return "Person [name=" + name + ", age=" + age + ", gender=" + gender + "]";
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(age, gender, name);
 	}
 	
 	@Override
 	public boolean equals(Object obj) {
-		Person person = (Person)obj;
-		if(name.equals(person.getName()) &&
-				age == person.getAge() &&
-				gender == person.getGender()) {
+		if (this == obj)
 			return true;
-		}
-		return false;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		return age == other.age && gender == other.gender && Objects.equals(name, other.name);
 	}
-
 }
