fc48c7c458813586f0f5187f9f0253724477c080
seoanchoi <seoaneeee@naver.com>
2022-11-04 14:36:08 +0900

    컬렉션-리스트

---
+++ java_demo11/src/kr/co/demo01/Subject.java
@@ -0,0 +1,46 @@
+package kr.co.demo01;
+
+public class Subject implements Comparable<Subject> {
+
+	private String name;
+	private double score;
+	
+	public Subject(String name, double score) {
+		super();
+		this.name = name;
+		this.score = score;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double getScore() {
+		return score;
+	}
+	
+	public void setScore(double score) {
+		this.score = score;
+	}
+		
+	@Override
+	public String toString() {
+		return "Subject [name=" + name + ", score=" + score + "]";
+	}
+	
+	@Override
+	public int compareTo(Subject o) {
+		if(this.score > o.score) {
+			return 1;
+		} else if(this.score > o.score) {
+		return -1;
+		}
+		return 0;	
+	}
+}
+	
+
