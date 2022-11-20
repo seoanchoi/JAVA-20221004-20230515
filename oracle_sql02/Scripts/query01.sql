87d4e63287df6a039c931c99e8e486b5d20d8f52
seoanchoi <seoaneeee@naver.com>
2022-11-21 08:53:57 +0900

    컬럼에 주석 설정

--- oracle_sql02/Scripts/query01.sql
+++ oracle_sql02/Scripts/query01.sql
@@ -1,4 +1,12 @@
 /*
+ * - DML : 데이터 조작어, INSERT(테이블에 데이터 추가), UPDATE(테이블의 데이터 수정), DELETE(테이블의 데이터 삭제) 
+ *         SELECT(DQL 로 분류하는 경우도 있음)
+ * - DDL : 데이터 정의어, CAREATE(객체 생성),  ALTER(객체 수정), DROP(객체 삭제)
+ * - TCL : 트랜젝션 제어어, COMMIT(지금까지의 변경 내용 저장), ROLLBACK(이전 내역으로 되돌림)
+ */
+
+
+/*
  * DDL
  *    - 데이터 정의어로 데이터를 저장하고 다루기 위한 객체를 
  *      생성하고(CREATE) 수정하고(ALTER) 삭제(DROP)하는 구문을 말한다.
@@ -18,9 +26,9 @@
  * 
  * 데이터 타입
  *    CHAR(크기)         : 고정 크기(바이트) 문자 데이터
- *    CHAR(CHAR 크기)    : 문자를 기준으로 크기를 설정
+ *    CHAR(크기 CHAR)    : 문자를 기준으로 크기를 설정
  *    VARCHAR2(크기)     : 가변 크기(바이트) 문자 데이터, 최대 4000 바이트 저장가능, 한글은 3 바이트 
- *    VARCAHR2(CAHR 크기): 문자를 기준으로 크기를 설정
+ *    VARCAHR2(크기 CHAR): 문자를 기준으로 크기를 설정
  *    NUMBER            : 숫자 데이터(최대 40자리)
  *    NUMBER(길이)       : 숫자 데이터, 길이 지정 가능
  *    NUMBER(길이, 자릿수) : 숫자 데이터, 소수점 자릿수 까지 지정
@@ -37,7 +45,7 @@
  *    PRIMARY KEY : NULL, 중복값 저장을 모두 허용하지 않음(Record의 고유 식별값으로 사용하기 위해)
  *    FOREIGN KEY : 참조하는 테이블의 컬럼의 값이 존재하면 허용(외래키 : 다른 테이블의 값을 참조하여 관계를 형성)
  *    CHECK       : 지정한 값만 저장할 수 있음
- *    DEFAULT     : 기본값을 설정하여 데이터 추가 작업에 누랑기 되어 있어도 기본값이 저장되게 한다.
+ *    DEFAULT     : 기본값을 설정하여 데이터 추가 작업에 누락이 되어 있어도 기본값이 저장되게 한다.
  *                  (제약 조건은 아니지만 제약조건이 작성되는 부분에 사용되어 여기에 작성함)
  * 
  * 위의 제약 조건을 위반하는 데이터 추가/수정/삭제 작업이 이루어 지는 경우 "제약조건 위반" 이라는 에러가 발생하게 된다.
@@ -47,7 +55,7 @@
  *    - 컬럼 레벨  : 컬럼명, 데이터 타입 옆에 작성하는 방법
  *                 기본키, 유일키, 외래키, NOT NULL, CHECK, DEFAULT 작성가능
  *    - 테이블 레벨 : 컬럼명, 데이터 타입을 작성 후 동일 위치상에 작성하는 방법
- *                 기본키, 유일키, 외래키, ㅊCHECK 작성 가능
+ *                 기본키, 유일키, 외래키, CHECK 작성 가능
  */
 
 CREATE TABLE TEST1_T (
@@ -97,8 +105,6 @@ DROP TABLE REF_T;
 DELETE FROM TEST1_T WHERE COL_NAME3 = 'A';
 DELETE FROM REF_T WHERE REF_COL1 = 'A';
 
-
-
 /*
  * ALTER TABLE : 생성한 테이블의 이름을 변경하거나 컬럼명, 데이터 타입, 제약조건을 수정할 때 사용
  *               테이블에 데이터가 저장되어 있는 경우 수정 작업이 안될 수 있다.(데이터 타입)
@@ -140,5 +146,16 @@ ALTER TABLE TEST4 MODIFY COL4 DATE DEFAULT NULL;
 SELECT * FROM USER_CONSTRAINTS WHERE TABLE_NAME = 'TEST4';
 SELECT * FROM USER_TAB_COLUMNS WHERE TABLE_NAME = 'TEST4';
 
-DROP TABLE TEST3;
-DROP TABLE TEST4;
+
+/*
+ * 테이블 컬럼에 주석(Comment) 적용하기.
+ */
+CREATE TABLE TEST5(
+       COL1 NUMBER
+     , COL2 VARCHAR2(10)
+);
+
+COMMENT ON COLUMN TEST5.COL1 IS '컬럼에 주석을 설정 합니다.';
+COMMENT ON COLUMN TEST5.COL2 IS '이 컬럼은 문자 10 Byte를 저장할 수 있습니다.';
+
+SELECT * FROM USER_COL_COMMENTS WHERE TABLE_NAME = 'TEST5';
