eb7264be5e67055f4fb1d166eed932b2e2ba9e62
seoanchoi <seoaneeee@naver.com>
2022-12-23 15:12:44 +0900

    자바스크립트 엘리먼트 추가

--- static_web01/webapp/static/js/sample06.js
+++ static_web01/webapp/static/js/sample06.js
@@ -34,14 +34,14 @@ function addItem(form) {
     
     if(form.loc.value === "first") {
         list.prepend(li);
-    } else if(frameElement.loc.value === "last") {
+    } else if(form.loc.value === "last") {
         list.append(li);
     }
 
     form.item.value = ""; 
 }
 
-function addRow() {
+function addRow(form) {
     /* 
     테이블에 행을 추가하는 함수를 사용한 행에는 5열이 들어간다. 
     1. 사용자 입력으로 제목과 작성자를 받을 수 있으며
@@ -49,8 +49,69 @@ function addRow() {
     3. 작성일은 new Date() 객체를 사용하여 현재 날짜를 구하며
     4. 조회수는 기본 0으로 설정한다.
     */
+
+    var tbody = document.getElementById("table").lastElementChild;
+    var now = new Date();
+
+    var tr = createTableRow(5);
+    tr.children[0].innerText = tbody.children.length + 1;
+    tr.children[1].innerText = setAndClear(form.title);
+    tr.children[2].innerText = setAndClear(form.writer);
+    tr.children[3].innerText = [now.getFullYear(), (now.getMonth() + 1), now.getDate()].join("-");
+    tr.children[4].innerText = 0;
+    tbody.append(tr);
+    }
+
+    function setAndClear(inputElement) {
+    var value = inputElement.value;
+    inputElement.value = "";
+    return value;
+    }
+
+    function createTableRow(columnCount) {
+    var row = document.createElement("tr");
+    for(let idx = 0; idx < columnCount; idx++) {
+        let column = document.createElement("td");
+        row.append(column);
+    }
+    return row;
+    }
+
+function validCheck() {
+/* 
+    1. 동일한 패스워드를 사용하는지 검사 후 동일하지 않은 값을 입력한 경우
+    패스워드 확인 입력 양식 옆에 "다시 입력하시오." 라는 메시지가 나오도록 하세요.
+    2. username 에서 6자리 이상의 아이디가 아닌 경우 아이디 입력 양식 옆에 
+    "6자리 이상 입력하시오."라는 메시지가 나오도록 한다.    
+*/
+    var isValid = true;
+    if(form.username.nextElementSibling !== null) {
+        form.username.nextElementSibling.remove();
+    }
+    if(form.username.value.length < 6) {
+        spanErrorMessage(form.username, "6자리 이상 입력하시오.");
+        form.username.value = "";
+        isValid = false;
+    }
+    if(form.passwordCheck.nextElementSibling !== null) {
+        form.passwordCheck.nextElementSibling.remove();
+    }
+    if(form.password.value !== form.passwordCheckvalue) {
+        spanErrorMessage(form.passwordCheck, "다시 입력하시오.");
+        form.passwordCheck.value = "";
+        isValid = false;
+    }
+    return isValid;
 }
 
-var element = document.getElementById("table")[0];
+function spanErrorMessage(element, message) {
+    let span = document.createElement("span");
+    span.innerText = message;
+    element.after(span);
+    return span;
+}
 
-tbody 
+
+
+
+
