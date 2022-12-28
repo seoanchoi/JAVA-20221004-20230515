fa2206563d83497168615ca2a7a8ac68f6160cde
seoanchoi <seoaneeee@naver.com>
2022-12-28 14:35:37 +0900

    JQuery 사용 예시

---
+++ static_web01/webapp/static/js/sample10.js
@@ -0,0 +1,51 @@
+/* $(document).ready(function() {
+    $("from > button").on("이벤트명", 메서드)
+}); */
+
+
+
+$(document).ready(function() {
+    $("form > button").on("click", addBookmark); // 이벤트 등록
+    // $("form > button").off("click", addBookmark); 이벤트 삭제
+    // $("form > button").one("click", addBookmark); 한번만 동작하는 이벤트로 등록
+    /*
+    $("ul a").hover(
+        function(e) {
+            var href = $(e.target).attr("href");
+            var name = $(e.target).text();
+            $("form [name=url]").val(href);
+            $("form [name=name]").val(name);
+        },
+        function(e) {
+            $("form [name=url]").val("");
+            $("form [name=name]").val("");
+        }
+    );
+    */
+   /*
+    $("form > button").on("click",
+        function(e) {
+            if($(e.target).attr("on") === "false") {
+                $(e.target).css("border-color", "red");
+                $(e.target).attr("on", "true");
+            } else {
+                $(e.target).removeAttr("style");
+                $(e.target).attr("on", "false");
+            }
+        }
+    );
+    */
+});
+
+function addBookmark(event) {
+    // var url = $("form").children().eq(0).val();
+    // var name = $("form").children().eq(1).val();
+    var url = $(event.target.form.url).val();
+    var name = $(event.target.form.name).val();
+
+    var aTag = $("<a></a>").attr("href", url).text(name);
+    var liTag = $("<li></li>");
+    liTag.append(aTag);
+    $(".bookmark-list").append(liTag);
+    $(event.target.form).children("input").val("");
+}
